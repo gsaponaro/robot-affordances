d5a915a7780566fa0b131df3a8bc38b17c06e45e
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2016-09-22 15:02:13 +0100

    [blobDescriptor2]: update copyright

--- src/blobDescriptor2/include/Obj2D.h
+++ src/blobDescriptor2/include/Obj2D.h
@@ -1,6 +1,5 @@
 /*
- * Copyright: (C) 2012-2015 POETICON++, European Commission FP7 project ICT-288382
- * Copyright: (C) 2015 VisLab, Institute for Systems and Robotics,
+ * Copyright: (C) 2016 VisLab, Institute for Systems and Robotics,
  *                Istituto Superior Técnico, Universidade de Lisboa, Lisbon, Portugal
  * Author: Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
  * CopyPolicy: Released under the terms of the GNU GPL v2.0
