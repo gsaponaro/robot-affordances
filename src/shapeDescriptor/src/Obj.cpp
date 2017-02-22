9c6fcd92bb74d20f4f923d2e908172a30508ff60
Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
2017-02-22 11:51:26 +0000

    update copyright and email

--- src/shapeDescriptor/src/Obj.cpp
+++ src/shapeDescriptor/src/Obj.cpp
@@ -1,7 +1,7 @@
 /*
- * Copyright: (C) 2016 VisLab, Institute for Systems and Robotics,
+ * Copyright: (C) 2017 VisLab, Institute for Systems and Robotics,
  *                Istituto Superior Técnico, Universidade de Lisboa, Lisbon, Portugal
- * Author: Giovanni Saponaro <gsaponaro@isr.ist.utl.pt>
+ * Author: Giovanni Saponaro <gsaponaro@isr.tecnico.ulisboa.pt>
  * CopyPolicy: Released under the terms of the GNU GPL v2.0
  *
  */
